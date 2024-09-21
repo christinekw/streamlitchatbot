ef6c5eb0fa8234a8e6fe30930575938c7e90e2fd
Sum Yi MA <ma.xinyi@outlook.com>
2024-09-21 16:44:07 +0800

    parameter

--- AIchatbot/model.py
+++ AIchatbot/model.py
@@ -8,9 +8,9 @@ from langchain_core.prompts import ChatPromptTemplate
 class Model(object):
     def __init__(self):
         # Load the model
-        self.llm_appraisal = ChatGroq(model="llama3-70b-8192",temperature=0.3,presence_penalty=1,
+        self.llm_appraisal = ChatGroq(model="llama3-70b-8192",temperature=0.7,presence_penalty=0.5,
                             api_key="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")
-        self.llm_response = ChatGroq(model="llama3-70b-8192",temperature=0.3,presence_penalty=1,
+        self.llm_response = ChatGroq(model="llama3-70b-8192",temperature=0.7,presence_penalty=0.5,
                             api_key="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")
         self.llm_summarize = ChatGroq(model="llama3-70b-8192",temperature=0.3,presence_penalty=0.3,
                             api_key="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")
