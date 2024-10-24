d25a488e62b29d6d5a1550a355a3f76e9cd7bf73
Sum Yi MA <ma.xinyi@outlook.com>
2024-10-24 19:43:19 +0800

    secret management

--- AIchatbot/model.py
+++ AIchatbot/model.py
@@ -2,18 +2,18 @@ from langchain_groq import ChatGroq
 import AIchatbot.appraisalprompt as appraisalprompt
 import AIchatbot.systemmsg as systemmsg
 from langchain_core.prompts import ChatPromptTemplate
-
+import streamlit as st
 
 
 class Model(object):
     def __init__(self):
         # Load the model
         self.llm_appraisal = ChatGroq(model="llama3-70b-8192",temperature=0.7,presence_penalty=0.5,
-                            api_key="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")
+                            api_key=st.secrets["api_key"])
         self.llm_response = ChatGroq(model="llama3-70b-8192",temperature=0.7,presence_penalty=0.5,
-                            api_key="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")
+                            api_key=st.secrets["api_key"])
         self.llm_summarize = ChatGroq(model="llama3-70b-8192",temperature=0.3,presence_penalty=0.3,
-                            api_key="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")
+                            api_key=st.secrets["api_key"])
         
         self.appraisalprompt = appraisalprompt.appra_prompt
         self.responsesystemprompt= ("system",systemmsg.sys)
